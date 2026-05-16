21f789591783d2ed7f080f948d28edbc8383ecfa
Wan Hua Wu <wanhuawoo@gmail.com>
2026-05-16 10:22:16 -0400

    Added authentication functions

--- src/app.js
+++ src/app.js
@@ -4,6 +4,10 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
+const compression = require('compression');
+const passport = require('passport');
+const authenticate = require('./auth');
+
 const logger = require('./logger');
 const pino = require('pino-http')({
   // Use our default logger instance, which is already configured
@@ -21,6 +25,13 @@ app.use(helmet());
 
 // Use CORS middleware so we can make requests across origins
 app.use(cors());
+
+// Use gzip/deflate compression middleware
+app.use(compression());
+
+// Set up our passport authentication middleware
+passport.use(authenticate.strategy());
+app.use(passport.initialize());
 
 // Define our routes
 app.use('/', require('./routes'));
